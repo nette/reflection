a3d2d080e2131a0c4c9efd999b1057b5a073de93
David Grudl <david@grudl.com>
2014-07-07 22:45:20 +0200

    marked @internal method

--- src/Reflection/AnnotationsParser.php
+++ src/Reflection/AnnotationsParser.php
@@ -270,6 +270,7 @@ class AnnotationsParser
 	 * Parses PHP file.
 	 * @param  string
 	 * @return array [class => [prop => comment (or 'use' => [alias => class])]
+	 * @internal
 	 */
 	public static function parsePhp($code)
 	{
