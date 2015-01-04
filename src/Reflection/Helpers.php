d34783388c54e0fb077610db0065fcab7eff31ed
Jáchym Toušek <enumag@gmail.com>
2015-01-04 19:39:39 +0100

    Removed support for methods, simplified the code

--- src/Reflection/Helpers.php
+++ src/Reflection/Helpers.php
@@ -18,26 +18,11 @@ class Helpers
 	/**
 	 * Unlike the PHP's getDeclaringClass method, this can return a trait reflection.
 	 * Results from this method should be cached.
-	 * @param \ReflectionProperty|\ReflectionMethod
 	 * @return Nette\Reflection\ClassType
 	 */
-	public static function getDeclaringClass(\Reflector $reflection)
+	public static function getDeclaringClass(\ReflectionProperty $reflection)
 	{	
-		$class = $reflection->getDeclaringClass();
-
-		if ($reflection instanceof \ReflectionProperty) {
-			$trait = self::searchTraitsForProperty($class, $reflection->getName());
-
-		} else {
-			$methodFile = $reflection->getFileName();
-			$methodStartLine = $reflection->getStartLine();
-			$methodEndLine = $reflection->getEndLine();
-			$trait = self::searchTraitsForMethod($class, $methodFile, $methodStartLine, $methodEndLine);
-		}
-
-		if ($trait !== NULL) {
-			$class = $trait;
-		}
+		$class = self::searchTraitsForProperty($reflection->getDeclaringClass(), $reflection->getName());
 
 		return new ClassType($class->getName());
 	}
@@ -49,31 +34,13 @@ class Helpers
 	private static function searchTraitsForProperty(\ReflectionClass $class, $name)
 	{
 		foreach ($class->getTraits() as $trait) {
-			$result = self::searchTraitsForProperty($trait, $name);
-			if ($result !== NULL) {
+			if ($result = self::searchTraitsForProperty($trait, $name)) {
 				return $result;
-
-			} elseif ($trait->hasProperty($name)) {
-				return $trait;
 			}
 		}
-	}
 
-
-	/**
-	 * Recursive method called to find a method in traits.
-	 */
-	private static function searchTraitsForMethod(\ReflectionClass $class, $methodFile, $methodStartLine, $methodEndLine)
-	{
-		foreach ($class->getTraits() as $trait) {
-			if ($trait->getFileName() === $methodFile && $trait->getStartLine() <= $methodStartLine && $trait->getEndLine() >= $methodEndLine) {
-				return $trait;
-			}
-
-			$result = self::searchTraitsForMethod($trait, $methodFile, $methodStartLine, $methodEndLine);
-			if ($result !== NULL) {
-				return $result;
-			}
+		if ($class->hasProperty($name)) {
+			return $class;
 		}
 	}
 
--- tests/Reflection/Helpers.getDeclatingClass.phpt
+++ tests/Reflection/Helpers.getDeclatingClass.phpt
@@ -14,21 +14,17 @@ require __DIR__ . '/../bootstrap.php';
 trait A
 {
 	protected $bar;
-	protected function bar() {}
-	protected function override() {}
 }
 
 trait B
 {
 	use A;
 	protected $foo;
-	protected function foo() {}
 }
 
 trait E
 {
 	protected $baz;
-	protected function baz() {} 
 }
 
 class C
@@ -36,8 +32,6 @@ class C
 	use B;
 	use E;
 	protected $own;
-	protected function own() {}
-	protected function override() {}
 }
 
 class D extends C
@@ -59,24 +53,4 @@ test(function() { // Property in class itself
 
 test(function() { // Property in second trait
 	Assert::same('E', Reflection\Helpers::getDeclaringClass(new \ReflectionProperty('D', 'baz'))->getName());
-});
-
-test(function() { // Method in trait
-	Assert::same('B', Reflection\Helpers::getDeclaringClass(new \ReflectionMethod('D', 'foo'))->getName());
-});
-
-test(function() { // Method in parent trait
-	Assert::same('A', Reflection\Helpers::getDeclaringClass(new \ReflectionMethod('D', 'bar'))->getName());
-});
-
-test(function() { // Method in class itself
-	Assert::same('C', Reflection\Helpers::getDeclaringClass(new \ReflectionMethod('D', 'own'))->getName());
-});
-
-test(function() { // Method in second trait
-	Assert::same('E', Reflection\Helpers::getDeclaringClass(new \ReflectionMethod('D', 'baz'))->getName());
-});
-
-test(function() { // Method in trait overridden by class
-	Assert::same('C', Reflection\Helpers::getDeclaringClass(new \ReflectionMethod('D', 'override'))->getName());
 });
